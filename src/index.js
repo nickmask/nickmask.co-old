e4971c32d061188eb2fcf40d00f6f0de8b45d338
Nick Maskell <nrmaskell@gmail.com>
2016-04-04 16:06:00 +1200

    Sass and img setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React = require('react')
+var ReactDOM = require('react-dom')
+require('./src/stylesheets/bootstrap.css')
+require('./src/stylesheets/main.sass')
 
-ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('root'));
+var img = document.createElement('img')
+img.src = require('./glyph.png')
+
+
+ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,8 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
+        test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192',
+        test: /\.sass$/, loader: 'style-loader!sass-loader!sass-loader',
         loader: 'babel-loader',
         query: {
           presets: ['es2015', 'react']
