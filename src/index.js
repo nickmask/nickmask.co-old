f97c524138745c2731b5787bd060446faba3cde4
Nick Maskell <nrmaskell@gmail.com>
2016-04-04 16:50:45 +1200

    Add working base setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
-require('./src/stylesheets/bootstrap.css')
-require('./src/stylesheets/main.sass')
+require('./stylesheets/main.sass')
 
 var img = document.createElement('img')
-img.src = require('./glyph.png')
+img.src = require('./images/pig.jpg')
 
 
 ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,46 @@
-module.exports = {
-  entry: './src/index',
-  output: {
-    filename: './public/bundle.js'
+const webpack = require('webpack')
+const autoprefixer = require('autoprefixer')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path')
+
+const sassLoaders = [
+  'css-loader',
+  'postcss-loader',
+  'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, './src')
+]
+
+const config = {
+  entry: {
+    app: ['./src/index']
   },
-  devtool: 'source-map',
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192',
-        test: /\.sass$/, loader: 'style-loader!sass-loader!sass-loader',
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react&presets[]=stage-0'
       },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+      },
+      { test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192'
+      }
     ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, './public'),
+    publicPath: '/'
+  },
+  devtool: 'inline-sourcemap',
+  plugins: [
+    new ExtractTextPlugin('bundle.css')
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.sass', '.css'],
+    modulesDirectories: ['src', 'node_modules']
   }
-};
+}
+
+module.exports = config
