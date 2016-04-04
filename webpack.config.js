1f575490a60586c8efd075f10204b6d09b6e1889
Nick Maskell <nrmaskell@gmail.com>
2016-04-04 15:45:50 +1200

    Add basic react template

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  entry: './index',
+  entry: './src/index',
   output: {
-    filename: 'browser-bundle.js'
+    filename: './public/bundle.js'
   },
   devtool: 'source-map',
   module: {
