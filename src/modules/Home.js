e4ce53f93768663184f3eccc761ad6bfcf63d69e
Nick Maskell <nrmaskell@gmail.com>
2016-04-15 12:21:24 +1200

    Add base homepage layout

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -1,20 +1,64 @@
 import React from 'react'
-import { Col, Row } from 'react-bootstrap'
+import { Col, Row, Image } from 'react-bootstrap'
 
 class Home extends React.Component {
   render () {
     return (
-      <Row fluid>
-        <Col md={4} fluid>
-          <img src='http://fillmurray.com/600/200'/>
-        </Col>
-        <Col md={8} fluid>
-          <div>
-            <h3>Hi, I'm Nick.</h3>
-            <p>I am a web developer with a love and passion for UX, design, tech and marketing. </p>
-          </div>
-        </Col>
-      </Row>
+      <div>
+        <Row className='show-grid'>
+          <Col md={3} fluid>
+            <Image src='http://fillmurray.com/300/200' responsive/>
+          </Col>
+          <Col md={9} fluid>
+            <div fluid>
+              <h3>Hi, I'm Nick.</h3>
+              <p>I am a web developer with a love and passion for UX, design, marketing and tech. I love making beautiful, intuitive products that help people and business become more efficent.</p>
+            </div>
+          </Col>
+        </Row>
+        <Row className='show-grid'>
+          <Col md={3} sm={6} fluid>
+            <h1>Latest projects</h1>
+          </Col>
+          <Col md={3} sm={6} fluid>
+            <Image src='http://fillmurray.com/300/200' responsive />
+          </Col>
+          <Col md={3} sm={6} fluid>
+            <Image src='http://fillmurray.com/300/200' responsive />
+          </Col>
+          <Col md={3} sm={6} fluid>
+            <Image src='http://fillmurray.com/300/200' responsive />
+          </Col>
+        </Row>
+        <Row className='show-grid'>
+          <Col md={3} sm={6} fluid>
+            <h1>Latest blog posts</h1>
+          </Col>
+          <Col md={3} sm={6} fluid>
+            <Image src='http://fillmurray.com/300/200' responsive />
+          </Col>
+          <Col md={3} sm={6} fluid>
+            <Image src='http://fillmurray.com/300/200' responsive />
+          </Col>
+          <Col md={3} sm={6} fluid>
+            <Image src='http://fillmurray.com/300/200' responsive />
+          </Col>
+        </Row>
+        <Row className='show-grid'>
+          <Col md={3} sm={3} fluid>
+            <h1>Contact</h1>
+          </Col>
+          <Col md={3} sm={3} fluid>
+            <Image src='http://fillmurray.com/300/200' responsive/>
+          </Col>
+          <Col md={3} sm={3} fluid>
+            <Image src='http://fillmurray.com/300/200' responsive />
+          </Col>
+          <Col md={3} sm={3} fluid>
+            <Image src='http://fillmurray.com/300/200' responsive />
+          </Col>
+        </Row>
+      </div>
     )
   }
 }
