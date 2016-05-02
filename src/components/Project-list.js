f992dbd842975d8f7e26289f46a62d2fe0686d9c
Nick Maskell <nrmaskell@gmail.com>
2016-05-02 19:12:17 +1200

    Semi-fix state in projectlist

--- src/components/Project-list.js
+++ src/components/Project-list.js
@@ -1,32 +1,60 @@
 import React from 'react'
 import { Col } from 'react-bootstrap'
+import request from 'superagent'
+import Firebase from 'firebase'
+import createFragment from 'react-addons-create-fragment'
+import _ from 'lodash'
 
 class ProjectList extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {list: []}
+  }
+
+  componentDidMount() {
+    let projects = new Firebase('https://nickmask.firebaseio.com/projects')
+    const self = this
+    let arr = []
+    projects.once('value', function (project) {
+      project.forEach(function (childProject) {
+        arr.push({
+          projectName: childProject.key(),
+          projectData: childProject.val(),
+        })
+      })
+      console.log('setting state', arr)
+      // self.setState = { list: arr }
+    })
+    this.setState = { list: arr }
+    console.log('this', this.state)
+    console.log('done once')
+  }
+
   render () {
-    return (
-      <div>
-        <Col sm={4} className='projectItem'>
-          <a href='/project/1'>
-            <div className='project'>
-              <h2 className='projectTitle'>Tightrope</h2>
-              <p className='projectInfo'>Node.js | React | Sass | Express</p>
-            </div>
-          </a>
-        </Col>
-        <Col sm={4} className='projectItem'>
-          <div className='project'>
-          </div>
-        </Col>
-        <Col sm={4} className='projectItem'>
-          <div className='project'>
-          </div>
-        </Col>
-        <Col sm={4} className='projectItem'>
-          <div className='project'>
-          </div>
-        </Col>
-      </div>
-    )
+    if (this.state.list.length <= 0) {
+      console.log('State is 0')
+      return (<div />)
+    } else {
+      console.log('State is something')
+      return (
+        <div>
+          { _.map(this.state.list, function (project) {
+            return (
+              <Col sm={4} key={project.projectName.replace(' ', '')} className='projectItem'>
+                <a href='/project/1'>
+                  <div className='project'>
+                    <h2 className='projectTitle'>{project.projectName}</h2>
+                    <p className='projectInfo'>{project.projectData.tech}</p>
+                  </div>
+                </a>
+              </Col>
+            )
+          })
+          }
+        </div>
+      )
+    }
   }
 }
 
