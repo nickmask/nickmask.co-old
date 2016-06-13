c19191221ce24869519493d313bcbe99b5f14963
Nick Maskell <nrmaskell@gmail.com>
2016-06-13 19:21:57 +1200

    Add redux actions for fetching data

---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+import fetch from 'isomorphic-fetch'
+
+export const REQUEST_PROJECTS = 'REQUEST_PROJECTS'
+export const RECEIVE_PROJECTS = 'RECEIVE_PROJECTS'
+
+export function requestProjects () {
+  return {
+    type: REQUEST_PROJECTS
+  }
+}
+
+export function receiveProjects (json) {
+  return {
+    type: RECEIVE_PROJECTS
+  }
+}
+
+export function fetchProjects () {
+  return function (dispatch) {
+    dispatch(requestProjects())
+    return fetch('https://nickmask.firebaseio.com/projects.json')
+      .then(response => response.json())
+      .then(json =>
+        dispatch(receiveProjects(json)))
+  }
+}
