81370f585aaa20e238775be83513c7dd28685949
Nick Maskell <nrmaskell@gmail.com>
2016-06-13 19:28:16 +1200

    Add redux reducer for fetching data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,24 @@
 import { combineReducers } from 'redux'
+import { REQUEST_PROJECTS, RECEIVE_PROJECTS } from '../actions/index'
 
 const rootReducer = combineReducers({
-
+  projects
 })
+
+function projects (state = { isFetching: false, projects: [] }, action) {
+  switch (action.type) {
+    case REQUEST_PROJECTS:
+      return Object.assign({}, state, {
+        isFetching: true
+      })
+    case RECEIVE_PROJECTS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        projects: action.projects
+      })
+    default:
+      return state
+  }
+}
 
 export default rootReducer
