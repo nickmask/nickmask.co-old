4607527568219978b6134e41b309387e8c9fdba0
Nick Maskell <nrmaskell@gmail.com>
2016-06-15 21:54:13 +1200

    Add fontawesome links

--- src/modules/About.js
+++ src/modules/About.js
@@ -1,14 +1,14 @@
 import React, { PropTypes, Component } from 'react'
 import { Row, Col, Button, Image } from 'react-bootstrap'
+import FontAwesome from 'react-fontawesome'
 require('../www/stylesheets/about.sass')
-
 
 class About extends Component {
   render () {
     return (
       <div className='aboutPage'>
         <Row>
-          <Col sm={6} className='aboutMainCol'>
+          <Col sm={8} className='aboutMainCol'>
             <div className='aboutMain'>
               <h1 className='intro'>
                 Hey, I'm Nick.
@@ -24,18 +24,21 @@ class About extends Component {
               </p>
             </div>
           </Col>
-          <Col sm={6} className='aboutMainCol'>
+          <Col sm={4} className='aboutMainCol'>
             <div className='contactLinks'>
-              <h1>github</h1>
-              <h1>linkedin</h1>
-              <h1>twitter</h1>
-              <h1>facebook</h1>
+              <h1><FontAwesome name='github'/> github</h1>
+              <h1><FontAwesome name='linkedin'/> linkedin</h1>
+              <h1><FontAwesome name='twitter'/> twitter</h1>
+              <h1><FontAwesome name='facebook'/> facebook</h1>
             </div>
           </Col>
         </Row>
         <Row>
-          <Col>
-
+          <Col sm={12} className='care'>
+          <hr/>
+            <h1>
+              What I care about
+            </h1>
           </Col>
         </Row>
       </div>
