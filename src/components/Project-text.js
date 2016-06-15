24f89679903d7db6467449bd0077e841d378a679
Nick Maskell <nrmaskell@gmail.com>
2016-06-15 14:50:16 +1200

    Get new json format working

--- src/components/Project-text.js
+++ src/components/Project-text.js
@@ -4,17 +4,27 @@ import { Image } from 'react-bootstrap'
 require('../www/stylesheets/projects.sass')
 
 export default class Text extends Component {
+
+  displayText () {
+    const project = this.props.project.text
+    const projectText = []
+    for(var i = 0; i < project.length; i++) {
+      console.log('Project', project[i])
+      return (
+        <div>
+          <h2>{project[i].text}</h2>
+          <p>{project[i].title}</p>
+        </div>
+      )
+    }
+  }
+
   render () {
     const { project, keys } = this.props
-    console.log(project.text)
+    console.log('running')
     return (
       <div>
-        {keys.map((key, i) =>
-          <div>
-            <h2>{Object.keys(key.i)}</h2>
-            <p>{Object.keys(project.i)}</p>
-          </div>
-        )}
+        {this.displayText()}
       </div>
     )
   }
