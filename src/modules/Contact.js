9e77731f3a7d18f2031451e072d7a5157d637c92
Nick Maskell <nrmaskell@gmail.com>
2016-06-16 15:26:49 +1200

    contact page looks good, just need right side

--- src/modules/Contact.js
+++ src/modules/Contact.js
@@ -1,14 +1,28 @@
 import React, { PropTypes, Component } from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Image } from 'react-bootstrap'
+import FontAwesome from 'react-fontawesome'
 require('../www/stylesheets/contact.sass')
 
 class Contact extends Component {
   render () {
     return (
-      <Row >
-        <Col sm={12} className='contactMainCol'>
+      <Row className='contact'>
+        <Col sm={6} className='contactMainColLeft'>
           <div className='contactMain'>
-            <h1>Coming soon...</h1>
+            <h1>Wanna get in touch?</h1>
+            <h2>Give me an email or hit me up on social media.</h2>
+            <div className='contactSocial'>
+              <a href='mailto:nrmaskell@gmail.com'><FontAwesome name='envelope'/></a>
+              <a href='https://github.com/nickmask'><FontAwesome name='github'/></a>
+              <a href='https://nz.linkedin.com/in/nickmask'><FontAwesome name='linkedin'/></a>
+              <a href='https://twitter.com/nickmask'><FontAwesome name='twitter'/></a>
+              <a href='https://www.facebook.com/nrmaskell'><FontAwesome name='facebook'/></a>
+            </div>
+
+          </div>
+        </Col>
+        <Col sm={6} className='contactMainColRight'>
+          <div className='contactMainRight'>
           </div>
         </Col>
       </Row>
--- src/modules/Home.js
+++ src/modules/Home.js
@@ -9,11 +9,11 @@ class Home extends Component {
       <div>
         <Row fluid>
           <Col className='gridItem homeContact' md={3} fluid>
-            <div>
-              <h1><FontAwesome name='github'/></h1>
-              <h1><FontAwesome name='linkedin'/></h1>
-              <h1><FontAwesome name='twitter'/></h1>
-              <h1><FontAwesome name='facebook'/></h1>
+            <div className='homeContactDiv'>
+              <a href='https://github.com/nickmask'><FontAwesome name='github'/></a>
+              <a href='https://nz.linkedin.com/in/nickmask'><FontAwesome name='linkedin'/></a>
+              <a href='https://twitter.com/nickmask'><FontAwesome name='twitter'/></a>
+              <a href='https://www.facebook.com/nrmaskell'><FontAwesome name='facebook'/></a>
             </div>
           </Col>
           <Col className='gridIntro' md={9} fluid>
