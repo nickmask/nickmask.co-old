01f8df94f3dee9da17dda994c3b018b18cd0bf0a
Nick Maskell <nrmaskell@gmail.com>
2016-06-16 12:35:33 +1200

    Base layout rejigged, css minimised

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -6,78 +6,40 @@ class Home extends React.Component {
   render () {
     return (
       <div>
-        <Row className='show-grid row1' fluid>
-          <Col className='gridItem' md={6} fluid>
-            <div className='col1'>
+        <Row fluid>
+          <Col className='gridItem' md={3} fluid>
+            <div>
+              <h1>About me</h1>
             </div>
           </Col>
-          <Col className='gridItem' md={6} fluid>
-            <div fluid>
-              <h3>Hi, I'm Nick.</h3>
-              <p>I am a web developer with a love and passion for UX, design, marketing and tech. I love making beautiful, intuitive products that help people and business become more efficent.</p> <br/>
-              <Button className='homeBut' bsStyle='primary'>Learn more</Button>
+          <Col className='gridIntro' md={9} fluid>
+            <div fluid className='homeIntro'>
+              <h1>Hey, I'm Nick.</h1>
+              <p>
+                I am a web developer with a love and passion for UX, design, marketing and tech. I love making beautiful, intuitive products that help people and business become more efficent.
+                <br/>
+                <Button className='homeBut' bsStyle='primary'>Learn more</Button>
+              </p>
+            </div>
+          </Col>
+          <Col  className='gridItem' sm={9} fluid>
+            <div className='projects'>
+              <h1>Latest projects</h1>
+            </div>
+          </Col>
+          <Col className='gridItem' sm={3} fluid>
+            <div className='col2'>
+            </div>
+          </Col>
+          <Col className='gridItem' sm={6} fluid>
+            <div className='col3' >
+            </div>
+          </Col>
+          <Col className='gridItem' sm={6} fluid>
+            <div className='col1' >
             </div>
           </Col>
         </Row>
-        <div className='homeGrid'>
-          <Row className='show-grid row2'>
-            <Col  className='gridItem' md={3} sm={6} fluid>
-              <div className='projects'>
-                <h1>Latest projects</h1>
-              </div>
-            </Col>
-            <Col className='gridItem' md={3} sm={6} fluid>
-              <div className='col2'>
-              </div>
-            </Col>
-            <Col className='gridItem' md={3} sm={6} fluid>
-              <div className='col3' >
-              </div>
-            </Col>
-            <Col className='gridItem' md={3} sm={6} fluid>
-              <div className='col1' >
-              </div>
-            </Col>
-          </Row>
-          <Row className='show-grid row3'>
-            <Col className='gridItem' md={3} sm={6} fluid>
-              <div className='blog'>
-                <h1>Latest blog posts</h1>
-              </div>
-            </Col>
-            <Col className='gridItem' md={3} sm={6} fluid>
-              <div className='col1'>
-              </div>
-            </Col>
-            <Col className='gridItem' md={3} sm={6} fluid>
-              <div className='col2'>
-              </div>
-            </Col>
-            <Col className='gridItem' md={3} sm={6} fluid>
-              <div className='col3'>
-              </div>
-            </Col>
-          </Row>
-          <Row className='show-grid row4'>
-            <Col className='gridItem' md={3} sm={3} fluid>
-              <div className='contact'>
-                <h1>Contact</h1>
-              </div>
-            </Col>
-            <Col className='gridItem' md={3} sm={3} fluid>
-              <div className='col2'>
-              </div>
-            </Col>
-            <Col className='gridItem' md={3} sm={3} fluid>
-              <div className='col1'>
-              </div>
-            </Col>
-            <Col className='gridItem' md={3} sm={3} fluid>
-              <div className='col3'>
-              </div>
-            </Col>
-          </Row>
-        </div>
       </div>
     )
   }
