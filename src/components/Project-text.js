cc66289d22749ee157be7c831a86ca19a7c10623
Nick Maskell <nrmaskell@gmail.com>
2016-06-19 19:57:51 +1200

    Half way through getting map of images

--- src/components/Project-text.js
+++ src/components/Project-text.js
@@ -1,7 +1,10 @@
 import React, { PropTypes, Component } from 'react'
 import { Link } from 'react-router'
 import { Image } from 'react-bootstrap'
+import Lightbox from 'react-images'
 require('../www/stylesheets/projects.sass')
+
+import Images from './Project-images'
 
 export default class Text extends Component {
 
@@ -12,17 +15,33 @@ export default class Text extends Component {
       return false
     }
   }
+
   render () {
-    const text = this.props.project.text
-    return (
-      <div>
-        {text.map((te, i) =>
+    const self = this
+    this.props.project.text.map(function (te, i) {
+      if (!self.hasImages(i)) {
+        console.log('first')
+        return (
           <div key={i}>
             <h2>{te.title}</h2>
             <p>{te.text}</p>
           </div>
-        )}
-      </div>
-    )
+        )
+      } else {
+        console.log('second')
+        return (
+          <div key={i}>
+            <h2>{te.title}</h2>
+            <p>{te.text}</p>
+            <Images images={te.images} />
+          </div>
+        )
+      }
+    })
   }
 }
+
+// {!this.hasBuild() && <div></div>
+// }
+// {this.hasBuild() && <Build build={this.props.project.built.split(", ")} />
+// }
