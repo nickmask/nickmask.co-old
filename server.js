858ce86674bf7e8baebdd5eb1728587b35f2df66
Nick Maskell <nrmaskell@gmail.com>
2016-06-20 20:14:10 +1200

    modify server paths

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require('express')
 module.exports = {
   app: function () {
     const app = express()
-    const indexPath = path.join(__dirname, './index.html')
-    const publicPath = express.static(path.join(__dirname, './build'))
+    const indexPath = path.join(__dirname, 'index.html')
+    const publicPath = express.static(path.join(__dirname, 'build'))
 
     app.use('/build', publicPath)
     app.get('/', function (_, res) { res.sendFile(indexPath) })
