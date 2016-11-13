f2f495421c1e4d729d0b7d9bd5f5281f6396f34f
nickmask <nrmaskell@gmail.com>
2016-11-13 15:36:12 +1300

    Add some old ass changes

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ import Project from './modules/Project'
 const loggerMiddleware = createLogger()
 
 const store = createStore(
-  rootReducer,
+  user,
   applyMiddleware(
     thunkMiddleware
   )
